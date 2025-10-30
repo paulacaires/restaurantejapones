6954618ea430a58ae8094c3e09ece609a843ac9e
JeanLucasR <jean@estudante.ufscar.br>
2025-10-30 17:20:30 -0300

    add: funcionarios module

--- backend/database/data_acess.py
+++ backend/database/data_acess.py
@@ -3,6 +3,7 @@ from supabase import Client
 # Nome da View (tabela virtual) que vamos consultar
 CLIENTES_VIEW = "vw_clientes_api" 
 CLIENTES_TABELA = "cliente"
+FUNCIONARIOS_TABELA = "funcionarios"
 
 def buscar_todos_clientes(supabase_client: Client):
     """
@@ -33,4 +34,17 @@ def adicionar_novo_cliente(supabase_client: Client, dados_cliente: dict):
         return response.data
     except Exception as e:
         print(f"Erro ao adicionar cliente: {e}")
+        return None
+    
+
+def buscar_todos_funcionarios(supabase_client: Client):
+    """
+    Função que busca todos os funcionários (implementação estática).
+    """
+    try:
+        # Consulta a TABELA 'funcionarios'
+        response = supabase_client.table(FUNCIONARIOS_TABELA).select("*").execute()
+        return response.data
+    except Exception as e:
+        print(f"Erro ao buscar funcionários: {e}")
         return None

a27e94d4a849577f271af153c12757bcedfafc50
Paula <mjpaulas@gmail.com>
2025-11-22 17:41:18 -0300

    remoção de lote no estoque

--- routes/estoque_routes.py
+++ routes/estoque_routes.py
@@ -10,7 +10,8 @@ from flask import (
 
 from backend.database.data_acess import (
     buscar_todos_itens_estoque,
-    buscar_itens_cardapio
+    buscar_itens_cardapio,
+    verificar_baixo_estoque
 )
 
 # Nome das tabelas no Supabase
@@ -29,44 +30,80 @@ def estoque():
     lista_estoque = buscar_todos_itens_estoque(supabase) or []
     itens_cardapio = buscar_itens_cardapio(supabase) or []
 
+    itens_alerta = verificar_baixo_estoque(lista_estoque)
+
     return render_template(
         "estoque.html",
         estoque=lista_estoque,
-        itens_cardapio=itens_cardapio
+        itens_cardapio=itens_cardapio,
+        itens_alerta=itens_alerta
     )
 
 @estoque_bp.route("/estoque/adicionar", methods=["POST"])
 def adicionar_item_estoque():
     supabase = current_app.supabase
+
     if not supabase:
         flash("Erro ao conectar ao banco de dados.", "erro")
         return redirect(url_for("estoque.estoque"))
 
     try:
         # Dados enviados pelo form
-        id_item       = request.form.get("id_item")
+        id_item         = request.form.get("id_item")
         quantidade      = request.form.get("quantidade")
         data_validade   = request.form.get("data_validade")
 
         # Validações básicas
         if not id_item or not quantidade or not data_validade:
-            flash("Por favor, selecione o item e informe a quantidade.", "erro")
             return redirect(url_for("estoque.estoque"))
-        
+
         payload = {
             "id_item": int(id_item),
             "quantidade": int(quantidade),
             "data_validade": data_validade
         }
 
-        response = supabase.table(ESTOQUE_TABELA).insert(payload).execute()
+        # Buscar se já existe um registro de estoque para esse item
+        response = (
+            supabase.table(ESTOQUE_TABELA)
+            .select("id_estoque, quantidade")
+            .eq("id_item", id_item)
+            .maybe_single()
+            .execute()
+        )
 
-        if response.data:
-            print(response.data[0])
-            return redirect(url_for("estoque.estoque"))
-    
-        return None  # deu algum erro silencioso
+        # Se já existe um registro no estoque para esse item
+        if response:
+            id_estoque = response.data["id_estoque"]
+            quantidade_atual = response.data["quantidade"]
 
+            nova_quantidade = int(quantidade_atual) + int(quantidade)
+
+            # Atualizar quantidade e sobreescreve a validade
+            update_resp = (
+                supabase.table(ESTOQUE_TABELA)
+                .update({
+                    "quantidade": nova_quantidade,
+                    "data_validade": data_validade
+                })
+                .eq("id_estoque", id_estoque)
+                .execute()
+            )
+
+        else:
+            # Não existe um registro para esse item em estoque
+            insert_resp = (
+                supabase.table(ESTOQUE_TABELA)
+                .insert({
+                    "id_item": id_item,
+                    "quantidade": quantidade,
+                    "data_validade": data_validade,
+                })
+                .execute()
+            )
+
+        return redirect(url_for("estoque.estoque"))
+        
     except Exception as e:
         print(f"Erro ao adicionar item ao estoque: {e}")
         return redirect(url_for("estoque.estoque"))

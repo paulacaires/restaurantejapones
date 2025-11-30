7212718f6b76e3bebf363ac2118b3429b7c42185
Paula <mjpaulas@gmail.com>
2025-11-30 19:25:31 -0300

    Corrigindo

--- backend/database/data_acess.py
+++ backend/database/data_acess.py
@@ -207,10 +207,16 @@ def buscar_todos_consumos(supabase_client: Client):
 
 def buscar_todas_reservas(supabase):
     """
-    Busca todas as reservas
+    Busca todas as reservas ordenando por dia e turno
     """
     try:
-        response = supabase.table("reserva_mesas").select("*, cliente(nome), mesas(lugares)").order("dia_reserva", desc=True).execute()
+        response = (
+            supabase.table("reserva_mesas")
+            .select("*, cliente(nome), mesas(lugares)")
+            .order("dia_reserva", desc=True)
+            .order("momento_refeicao")
+            .execute()
+        )
         return response.data
     except Exception as e:
         print(f"Erro ao buscar reservas: {e}")
@@ -221,13 +227,7 @@ def buscar_todas_mesas(supabase):
     Busca todas as mesas para preencher o formulário.
     """
     try:
-        response = (
-            supabase.table("reservas")
-            .select("*")
-            .order("dia_reserva", ascending=True)
-            .order("momento_refeicao", foreign_table=None, ascending=True)
-            .execute()
-        )
+        response = supabase.table("mesas").select("*").order("numero").execute()
         return response.data
     except Exception as e:
         print(f"Erro ao buscar mesas: {e}")
